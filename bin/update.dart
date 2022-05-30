c58470102166ba5b2f14526fbb05924cee5d08a7
Jeff Peiffer <jpeiffer@gmail.com>
2022-05-30 14:28:06 -0400

    -m

--- bin/update.dart
+++ bin/update.dart
@@ -42,7 +42,6 @@ Future<void> main(List<String>? args) async {
     );
     parser.addOption(
       'message',
-      defaultsTo: 'Automated dependency update',
     );
     parser.addOption(
       'paths',
